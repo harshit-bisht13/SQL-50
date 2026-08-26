4009016eedabb804214614fb031d50a91e195fb3
Hgafayagagag <harshitbisht1325@gmail.com>
2026-08-26 12:38:42 +0530

    Solve  1978 solution from leetcode.

---
+++ 1978 Employees whose manager left the company.sql
@@ -0,0 +1,5 @@
+
+Select e.employee_id  from Employees e 
+Left join Employees m
+on e.manager_id =m.employee_id
+where e.salary<30000 and e.manager_id is not  null and m .employee_id is null order by e.employee_id;
