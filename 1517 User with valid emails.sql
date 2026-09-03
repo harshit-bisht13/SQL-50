0cda199a4bb7b7dcb75cf814bd4ad477ee113e0b
Hgafayagagag <harshitbisht1325@gmail.com>
2026-09-03 07:20:53 +0530

    Solved leetcode solutions..

--- 1517 User with valid emails.sql
+++ 1517 User with valid emails.sql
@@ -1,4 +1,7 @@
-
 SELECT user_id, name, mail
 FROM Users
-WHERE mail REGEXP '^[A-Za-z][A-Za-z0-9_.-]*@leetcode[.]com$';
+WHERE REGEXP_LIKE(
+    mail,
+    '^[A-Za-z][A-Za-z0-9_.-]*@leetcode[.]com$',
+    'c'
+);
