c7145f40c98d0ffeb4745507d0469000c53eb8be
Hgafayagagag <harshitbisht1325@gmail.com>
2026-09-04 07:41:15 +0530

    Solve leetcode solution 1484

---
+++ 1484 group sold product by date.sql
@@ -0,0 +1,8 @@
+Select 
+    sell_date,
+    count(distinct product) as num_sold,
+    group_concat(Distinct product order by product ) 
+    as products 
+    from Activities 
+    group by sell_date 
+    order by sell_date;
